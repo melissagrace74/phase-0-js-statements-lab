c3bf80db2348d575296dc34519762532aee2448a
melissagrace74 <melissaggonzalez@sbcglobal.net>
2025-10-26 20:32:49 -0500

    Changed const to let for temperature and time of day.

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,27 @@
-const temperature = 90;
-const timeOfDay = "morning";
+let temperature = 90;
+let timeOfDay = "morning";
 let soilMoisture = 30;
+
+// Watering Control
+temperature = 85; 
+
+if (temperature > 80) {
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
+
+// Garden Lights Control
+if (timeOfDay === "evening" || timeOfDay === "night") {
+  console.log("Lights on");
+} else {
+  console.log("Lights off");
+}
+
+// Soil Moisture Adjustment
+soilMoisture = 0;
+
+while (soilMoisture < 40) {
+  soilMoisture += 5;
+  console.log(soilMoisture);
+}
